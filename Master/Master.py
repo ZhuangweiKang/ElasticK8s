dbda35d824c512f5e08b6a45c13089b777f950d7
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-05-11 13:44:40 -0500

    update

--- Master/Master.py
+++ Master/Master.py
@@ -10,7 +10,6 @@ from kubernetes import client, config
 image_name = 'zhuangweikang/streamingml_worker'
 container_port = 2341
 
-
 def create_deployment(deployment_name, pod_label, container_name):
     # Load config from default location
     config.load_kube_config()
@@ -49,6 +48,31 @@ def create_deployment(deployment_name, pod_label, container_name):
     extension.create_namespaced_deployment(namespace="default", body=deployment)
 
 
+# Create a service using NodePort manner
+# node_port starts from 30000
+def create_svc(svc_name, selector_label, _port, _node_port):
+    config.load_kube_config()
+    api_instance = client.CoreV1Api()
+    service = client.V1Service()
+
+    service.api_version = "v1"
+    service.kind = "Service"
+
+    # define meta data
+    service.metadata = client.V1ObjectMeta(name=svc_name)
+
+    # define spec part
+    spec = client.V1ServiceSpec()
+    spec.selector = {"app": selector_label}
+    spec.type = "NodePort"
+    spec.ports = [client.V1ServicePort(protocol="TCP", port=_port, target_port=_node_port)]
+
+    service.spec = spec
+
+    # create service
+    api_instance.create_namespaced_service(namespace="default", body=service)
+
+
 def build_socket():
     context = zmq.Context()
     socket = context.socket(zmq.REP)
@@ -59,22 +83,42 @@ def build_socket():
 def main():
     socket = build_socket()
     count = 0
+    node_port = 30000
     while True:
         msg = socket.recv_string()
         if msg == 'scale-in':
             count += 1
+            # create deployment here
             deployment_name = 'worker' + str(count) + '-deployment'
             pod_label = 'worker' + str(count)
             container_name = 'worker' + str(count)
             create_deployment(deployment_name, pod_label, container_name)
-            print('Create new deployment...')
+            print('Create new deployment: '+ deployment_name)
+
+            # create service here
+            svc_name = 'worker' + str(count) + '-service'
+            selector_label = pod_label
+            _port = 2341
+            node_port += 1
+            create_svc(svc_name, selector_label, _port, node_port)
+            print('Create new service: ' + svc_name)
+
         elif msg == 'scale-out':
+            # delete deployment
             deployment_name = 'worker' + str(count) + '-deployment'
             drop_deployment = 'kubectl delete deployment ' + deployment_name
-            print('Delete deployment ' + deployment_name)
             os.system(drop_deployment)
+            print('Delete deployment: ' + deployment_name)
             time.sleep(10)
             count -= 1
+
+            # delete service
+            svc_name = 'worker' + str(count) + '-service'
+            drop_svc = 'kubectl delete svc ' + svc_name
+            os.system(drop_svc)
+            print('Delete service: ' + svc_name)
+            node_port -= 1
+            
         else:
             print('Current max number is ' + msg)
         socket.send_string('Ack')
