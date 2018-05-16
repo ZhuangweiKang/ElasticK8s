8d5626cfb866424a6bd1d7520e13d64d7ffdc77f
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-05-16 16:28:31 -0500

    update

--- Master/TimeMeasureModel.py
+++ Master/TimeMeasureModel.py
@@ -117,8 +117,13 @@ def timeMeasurementExperiment(hasImage):
             crl.setopt(crl.POSTFIELDS, json.dumps(post_data_dic, ensure_ascii=False))
             crl.setopt(pycurl.URL, url)
             crl.setopt(crl.WRITEFUNCTION, crl.fp.write)
-            # crl.perform()
-            print(crl.getinfo(pycurl.HTTP_CODE))
+            while True:
+                try:
+                    crl.perform()
+                    print(crl.fp.getvalue()[0])
+                    break
+                except Exception:
+                    continue
 
             '''
             while True:
