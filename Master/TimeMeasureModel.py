2e66d07bc3d7a79b4532e4a8401fb9591a53ada7
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-05-17 11:49:58 -0500

    update

--- Master/TimeMeasureModel.py
+++ Master/TimeMeasureModel.py
@@ -10,6 +10,7 @@ import socket
 import pycurl
 import urllib
 import socket
+import simplejson
 import random
 from K8sOperations import K8sOperations as K8sOp
 
@@ -52,8 +53,8 @@ def timeMeasurementExperiment(hasImage, output_file, node_name, node_address):
             pod_label = node_name + '-' + str(random.randint(1, 1000)) + '-pod'
             image_name = images[j]
             container_name = pod_label
-            cpu_requests = '6.0'
-            cpu_limits = '6.0'
+            cpu_requests = '3.0'
+            cpu_limits = '4.0'
             start = datetime.datetime.now()
             k8sop.create_deployment(node_name, deployment_name, pod_label, image_name, container_name,  cpu_requests, cpu_limits, container_port=7000)
             print('Create deployment: %s' % deployment_name)
@@ -63,6 +64,14 @@ def timeMeasurementExperiment(hasImage, output_file, node_name, node_address):
             k8sop.create_svc(svc_name, selector_label)
             print('Create service: %s' % svc_name)
             
+            print('Waiting for container ready...')
+            while True:
+                command = 'kubectl get pods -o json'
+                _exec = os.popen(command)
+                data = simplejson.loads(_exec.read())
+                if len(data['items']) != 0 and data['items'][0]['']:
+                    break
+
             print('Waiting for container to load model...')
             
             while True:
